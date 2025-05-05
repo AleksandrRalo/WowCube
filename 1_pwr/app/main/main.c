819ae22e1afdc9851ced8ce2e0ead23463471f9b
ruslansukhov <sukhov@karazin.ua>
2025-05-05 22:01:14 +0300

    sample task added; sdkconfig removed from ignore list

--- 1_pwr/app/main/main.c
+++ 1_pwr/app/main/main.c
@@ -1,6 +1,24 @@
-#include <stdio.h>
+#include "freertos/FreeRTOS.h" 
+#include "freertos/task.h" 
+#include "driver/gpio.h" 
+#include <stdio.h> 
+
+void vTaskLed( void * pvParameters );
 
 void app_main(void)
 {
+    TaskHandle_t xHandle = NULL;
 
+    xTaskCreate( vTaskLed, "TaskLed", 2048, 0, tskIDLE_PRIORITY, &xHandle );
+    configASSERT( xHandle );
+}
+
+void vTaskLed( void * pvParameters )
+{
+  for( ;; )
+  {
+      // Task code goes here.
+      vTaskDelay(100);
+      printf("Hello\n\r");
+  }
 }
